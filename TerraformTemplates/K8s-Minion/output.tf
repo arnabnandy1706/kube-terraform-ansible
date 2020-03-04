2b00edf3e0824067b8c8c637c1a790e70456bac0
arnabnandy <arnab.nandy1991@gmail.com>
2020-03-04 16:13:14 +0530

    Added New Scripts

--- TerraformTemplates/K8s-Minion/output.tf
+++ TerraformTemplates/K8s-Minion/output.tf
@@ -1,3 +1,3 @@
 output "ec2instance" {
-  value = "IP: [${element(aws_instance.project-k8s.*.public_ip, 0)}, ${element(aws_instance.project-k8s.*.public_ip, 1)}]"
+  value = "IP:[${element(aws_instance.project-k8s.*.private_ip, 0)},${element(aws_instance.project-k8s.*.private_ip, 1)}]"
 }
--- TerraformTemplates/K8s-Minion/variables.tf
+++ TerraformTemplates/K8s-Minion/variables.tf
@@ -26,7 +26,7 @@ variable "awsprops" {
         region = "us-east-1"
         vpc = "vpc-5234832d"
         ami = "ami-0c1bea58988a989155"
-        itype = "t2.micro"
+        itype = "t2.medium"
         subnet = "subnet-81896c8e"
         publicip = true
         keyname = "myseckey"
